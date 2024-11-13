9a4bd993ac79b68339fef3f3905d7addbf089d82
to'raboyev xursand <xursandtoraboyev810@gmail.com>
2024-11-13 23:15:49 +0500

    api

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,18 @@ function App() {
     };
 
     const onSendData = useCallback(() => {
-        telegram.sendData(JSON.stringify(cartItems));
+        const queryId = telegram.initDataUnsave?.query_id;
+        if (queryId) {
+            fetch("http://localhost:8000/web-data", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(cartItems),
+            });
+        } else {
+            telegram.sendData(JSON.stringify(cartItems));
+        }
     }, [cartItems]);
     useEffect(() => {
         telegram.onEvent("mainButtonClicked", onSendData);
